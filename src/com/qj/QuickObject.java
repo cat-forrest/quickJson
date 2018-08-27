b14480616fdcf521711635425036e05e97652148
forrest <forrest.wamg@sunvalley.com.cn>
2018-08-27 21:12:10 +0800

    create app

---
+++ src/com/qj/QuickObject.java
@@ -0,0 +1,16 @@
+package com.qj;
+
+public class QuickObject {
+
+  private static String jsonOb;
+
+  public QuickObject(String jsonOb) {
+    this.jsonOb = jsonOb;
+  }
+
+  public static Object getObject() {
+
+    return null;
+  }
+
+}
